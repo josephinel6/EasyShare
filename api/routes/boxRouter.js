d8d2bec94f342e64959f9bde8911aaffe0436570
Josephine Li <josephinemouse@gmail.com>
2024-06-16 11:29:17 -0700

    fix verifying boxes

--- api/routes/boxRouter.js
+++ api/routes/boxRouter.js
@@ -1,124 +1,130 @@
-const express = require('express');
+const express = require("express");
 
 const route = express.Router();
 
-const Box = require("../models/box")
-const User = require("../models/user")
-const Share = require("../models/share")
+const Box = require("../models/box");
+const User = require("../models/user");
+const Share = require("../models/share");
 
-const cookieParser = require('cookie-parser')
-const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 route.use(cookieParser());
 
 const getUserFromToken = (req) => {
-    console.log("run");
-    return new Promise((resolve, reject) => {
-        jwt.verify(req.cookies.token, process.env.JWT_SECRET, {}, async (err, userData) => {
-            if (err) throw err;
-            const user = await User.findById(userData.id);
-            resolve(user);
-        });
-    });
-}
+  console.log("run");
+  return new Promise((resolve, reject) => {
+    jwt.verify(
+      req.cookies.token,
+      process.env.JWT_SECRET,
+      {},
+      async (err, userData) => {
+        if (err) throw err;
+        const user = await User.findById(userData.id);
+        resolve(user);
+      }
+    );
+  });
+};
 
-route.post('/create', async (req, res) => {
-    const { name } = req.body;
-    let code = Math.floor(Math.random() * 90000000 + 10000000)
-    const { token } = req.cookies;
-    const user = await getUserFromToken(req);
-    console.log(user);
-    let test = await Box.findOne({ code });
-    console.log(test);
-    while (test) {
-        console.log("finding" + code);
-        code = Math.floor(Math.random() * 90000000 + 10000000)
-        test = await Box.findOne({ code });
-    }
-    console.log("Creating");
-    try {
-        const box = await Box.create({
-            name: name,
-            code: code,
-            owner: user._id,
-        });
-        res.json(box);
-    } catch (err) {
-        console.log("Error");
-        res.json(err);
-    }
+route.post("/create", async (req, res) => {
+  const { name } = req.body;
+  let code = Math.floor(Math.random() * 90000000 + 10000000);
+  const { token } = req.cookies;
+  const user = await getUserFromToken(req);
+  console.log(user);
+  let test = await Box.findOne({ code });
+  console.log(test);
+  while (test) {
+    console.log("finding" + code);
+    code = Math.floor(Math.random() * 90000000 + 10000000);
+    test = await Box.findOne({ code });
+  }
+  console.log("Creating");
+  try {
+    const box = await Box.create({
+      name: name,
+      code: code,
+      owner: user._id,
+    });
+    res.json(box);
+  } catch (err) {
+    console.log("Error");
+    res.json(err);
+  }
 });
 
-route.get('/get-all', async (req, res) => {
-    // console.log("get all")
-    const user = await getUserFromToken(req);
-    const boxes = await Box.find({ owner: user._id });
-    // console.log(boxes);
-    res.json(boxes);
-})
+route.get("/get-all", async (req, res) => {
+  // console.log("get all")
+  const user = await getUserFromToken(req);
+  const boxes = await Box.find({ owner: user._id });
+  // console.log(boxes);
+  res.json(boxes);
+});
 
-route.post('/share', async (req, res) => {
-    const { share, code } = req.body;
-    console.log(req.body);
-    console.log(share);
-    console.log(code);
-    try {
-        const newShare = await Share.create({
-            share: share,
-            code: code
-        });
-        res.json(newShare);
-    } catch (err) {
-        console.log("Error");
-        console.log(err);
-        res.json(err);
-    }
-})
+route.post("/share", async (req, res) => {
+  const { share, code } = req.body;
+  console.log(req.body);
+  console.log(share);
+  console.log(code);
+  try {
+    const newShare = await Share.create({
+      share: share,
+      code: code,
+    });
+    res.json(newShare);
+  } catch (err) {
+    console.log("Error");
+    console.log(err);
+    res.json(err);
+  }
+});
 
-route.get('/shares/:code', async (req, res) => {
-    console.log("/shares/code");
-    const code = req.params['code'];
+route.get("/shares/:code", async (req, res) => {
+  console.log("/shares/code");
+  const code = req.params["code"];
 
-    console.log(code);
+  console.log(code);
 
-    const shares = await Share.find({ code });
+  const shares = await Share.find({ code });
 
-    res.json(shares);
-})
+  res.json(shares);
+});
 
-route.get('/verify', async (req, res) => {
-    const { code } = req.body;
-    if (await Box.find({ code })[0]) {
-        res.json(true);
-    } else {
-        res.json(false);
-    }
-})
+route.get("/verify/:code", async (req, res) => {
+  const code = req.params["code"];
+  //   const test = await Box.findOne({ code });
+  //   console.log(test);
+  if (await Box.findOne({ code })) {
+    res.json(true);
+  } else {
+    res.json(false);
+  }
+});
 
-route.post('/delete-share', async (req, res) => {
-    console.log(req.body);
-    const { id } = req.body;
+route.post("/delete-share", async (req, res) => {
+  console.log(req.body);
+  const { id } = req.body;
 
-    console.log(id);
+  console.log(id);
 
-    try {
-        const share = await Share.findByIdAndDelete(id);
-        res.json(share);
-    } catch (err) {
-        res.json(err);
-    }
-})
+  try {
+    const share = await Share.findByIdAndDelete(id);
+    res.json(share);
+  } catch (err) {
+    res.json(err);
+  }
+});
 
-route.post('/delete', async (req, res) => {
+route.post("/delete", async (req, res) => {
+  const { code } = req.body;
 
-    const { code } = req.body;
-
-    try {
-        const deleted = await Box.findOneAndDelete({ code });
-        res.json(deleted);
-    } catch (err) {
-        res.json(err);
-    }
-})
+  try {
+    const deleted = await Box.findOneAndDelete({ code });
+    res.json(deleted);
+  } catch (err) {
+    res.json(err);
+  }
+});
 
 module.exports = route;
--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,41 +1,59 @@
-import "../styles/welcome.css"
-import { Link } from "react-router-dom"
+import "../styles/welcome.css";
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import SubmitShare from "./SubmitShare";
 import Popup from "reactjs-popup";
 
 export default function Welcome() {
+  const [code, setCode] = useState("");
+  const [formOpen, setFormOpen] = useState(false);
 
-    const [code, setCode] = useState("");
-    const [formOpen, setFormOpen] = useState(false);
+  async function processClass() {
+    axios.get("/box/verify/" + code).then((response) => {
+      console.log(response.data);
+      if (response.data) {
+        setFormOpen(response.data);
+      } else {
+        alert("Box does not exist");
+      }
+    });
+  }
 
-    async function processClass() {
-        axios.get('/box/verify', { code: code })
-            .then((response) => {
-                console.log(response.data);
-                if (response.data) {
-                    setFormOpen(response.data);
-                } else {
-                    alert("Box does not exist");
-                }
-            })
-    }
+  const changeViewStatus = (status) => {
+    setFormOpen(status);
+  };
 
-    const changeViewStatus = (status) => {
-        setFormOpen(status);
-    }
-
-    return (
-        <div id="welcome-page" className="center">
-            <input id="enter-class-code" placeholder="00000000" onChange={e => setCode(e.target.value)} />
-            <br />
-            <div className="break"></div>
-            <button className="styled-button" id="enter-class" onClick={() => processClass()}> Enter class </button>
-            <br></br>
-            <Link to={'/register'} id="register" className="register-login"> Register</Link>
-            <Link to={'/login'} id="login" className="register-login"> Log in</Link>
-            <Popup open={formOpen}> <SubmitShare code={code} openCallback={changeViewStatus} /></Popup>
-        </div>
-    )
+  return (
+    <div id="welcome-page" className="center">
+      <input
+        id="enter-class-code"
+        placeholder="00000000"
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <br />
+      <div className="break"></div>
+      <button
+        className="styled-button"
+        id="enter-class"
+        onClick={() => processClass()}
+      >
+        {" "}
+        Enter class{" "}
+      </button>
+      <br></br>
+      <Link to={"/register"} id="register" className="register-login">
+        {" "}
+        Register
+      </Link>
+      <Link to={"/login"} id="login" className="register-login">
+        {" "}
+        Log in
+      </Link>
+      <Popup open={formOpen}>
+        {" "}
+        <SubmitShare code={code} openCallback={changeViewStatus} />
+      </Popup>
+    </div>
+  );
 }
