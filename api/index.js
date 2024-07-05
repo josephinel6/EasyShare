f4d0de7aadaceefbbd8434f6b1b8c67fb059a895
Josephine L <127054185+josephinel6@users.noreply.github.com>
2024-07-04 23:18:30 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    // credentials: true,
+    credentials: true,
     // origin: "http://localhost:3000",
      origin: "https://easy-share-site.vercel.app",
   })
